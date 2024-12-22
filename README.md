3deec1bb16a7d2abf4eae7e987b111c6bf8f472b
Dhruva Sharma <dhruva1098@gmail.com>
2024-12-22 18:33:36 +0530

    wrote a simple bootloader (sort of working? main is not printing everything it should"

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # uniX
+we will use a lba throughtout
+I will make conversion from chs as:
+    sectors = (LBA % sectors per track) + 1
+    head = (LBA / sectors per tracl) %heads
+    cylinder = (LBA / sectors per track ) / heads
--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -69,6 +69,13 @@ main:
     mov ss, ax
     mov sp, 0x7C00 ;stack starts at 0x7C00
     
+    ;read something from disk
+    mov [ebr_drive_number], dl
+    mov ax, 1
+    mov cl, 1
+    mov bx, 0x7E00
+    call disk_read
+
     ;print
     mov si, hello
     call puts
@@ -77,8 +84,100 @@ main:
 
 
 .halt:
-    jmp .halt
+    cli
+    hlt
+
+;handling floppy errors
+
+floppy_error:
+    mov si, msg_read_fail
+    call puts
+    jmp wait_key_and_reboot
+    
+wait_key_and_reboot:
+    mov ah, 0
+    int 16h
+    jmp 0ffffH:0
+
+;function for lba to chs conversion
+lba_to_chs:
+
+    push ax
+    push dx
+    xor dx, dx
+    div word [bdb_sectors_per_track]  ;dx = lba % sectors_per_track
+
+    inc dx
+    mov cx, dx  ;cx = dx + 1; cx = head
+    
+    xor dx, dx
+    div word [bdb_heads]  ;dx = lba % heads
+
+    mov dh, dl
+    mov ch, al  ;ch = cylinder
+    shl ah, 6
+    or cl, ah   ;cl = sector
+
+    pop ax
+    mov dl, al
+    pop ax
+    ret
+
+;read sectors from disk
+disk_read:
+
+    push ax
+    push bx
+    push cx
+    push dx
+    push di
+
+    push cx
+    call lba_to_chs
+    pop ax
+
+    mov ah, 02h
+    mov di, 3
+
+disk_reset:
+    pusha
+    mov ah, 0
+    stc
+    int 13h
+    jc floppy_error
+    popa
+    ret
+    
+.retry:
+    pusha
+    stc
+    int 13h
+    jnc .done
+
+    ;read failed
+    popa
+    call disk_reset
+
+    dec di
+    test di, di
+    jnz .retry
+
+.fail:
+    jmp floppy_error
+
+
+.done: 
+    popa
+    push di
+    push dx
+    push cx
+    push bx
+    push ax ;restore modified registers
+
+
 
 hello:db 'Hello world!', ENDL, 0
+msg_read_fail:db 'Read from the message disk failed', ENDL, 0
+
 times 510-($-$$) db 0
 dw 0AA55h
