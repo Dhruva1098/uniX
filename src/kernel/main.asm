b98b9a081c9dc6af58b25eab5853c85c470b7c3a
Dhruva Sharma <dhruva1098@gmail.com>
2025-01-06 12:31:19 +0530

    file read in x86asm
    this took literal dayS wow

--- src/kernel/main.asm
+++ src/kernel/main.asm
@@ -1,16 +1,24 @@
 ;interrtupts we use are AH
 ;STACK GROWS DOWNWARDS!!
-org 0x7C00
+org 0x0
 bits 16
 
 %define ENDL 0x0D, 0x0A
 start:
-    jmp main   
+
+    ;print
+    mov si, hello
+    call puts
+
+    cli
+    hlt
 
 
-;prints "Hello, World!" to the screen
-;Params: 
-;   -ds:si points to string
+.halt:
+    cli
+    hlt
+
+
 puts:
     push si
     push ax
@@ -26,31 +34,9 @@ puts:
     int 0x10        ;call interrupt
     jmp .loop     ;repeat
 
-.done:
+.done: 
     pop ax
     pop si
     ret 
 
-main:
-    
-    mov ax, 0
-    mov ds, ax
-    mov es, ax  ;setting up data segments, also cannto write in es,ds directly 
-    
-    ;setup stack
-    mov ss, ax
-    mov sp, 0x7C00 ;stack starts at 0x7C00
-    
-    ;print
-    mov si, hello
-    call puts
-
-    hlt
-
-
-.halt:
-    jmp .halt
-
-hello:db 'Hello world!', ENDL, 0
-times 510-($-$$) db 0
-dw 0AA55h
+hello: db "Hello, World from KERNEL!", ENDL, 0
